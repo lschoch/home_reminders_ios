92881cacbe1ad3179a0d4f4931d65b20429a49d2
Larry Schoch <lschoch@gmail.com>
2025-09-06 08:17:53 -0400

    Initial Commit

---
+++ Home RemindersTests/Home_RemindersTests.swift
@@ -0,0 +1,17 @@
+//
+//  Home_RemindersTests.swift
+//  Home RemindersTests
+//
+//  Created by Lawrence H. Schoch on 9/6/25.
+//
+
+import Testing
+@testable import Home_Reminders
+
+struct Home_RemindersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
