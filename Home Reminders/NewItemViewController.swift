4a36954baeda58984df8ecc971081b3fc27bfc5a
Larry Schoch <lschoch@gmail.com>
2025-09-06 23:30:45 -0400

    Rename ViewController to TableViewController, add NavigationViewController, add NewItemViewController

---
+++ Home Reminders/NewItemViewController.swift
@@ -0,0 +1,28 @@
+//
+//  NewItemViewController.swift
+//  Home Reminders
+//
+//  Created by Lawrence H. Schoch on 9/6/25.
+//
+
+import UIKit
+
+class NewItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
