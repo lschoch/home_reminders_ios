f8d0f797f4be8cf30d2f8e82c7664dfadb71d739
Larry Schoch <lschoch@gmail.com>
2025-09-13 12:30:12 -0400

    Rename TableViewController to RemindersViewController and update filename accordingly; rename Data Model directory to Models; add Reminders data model; implement datasource and delegate methods in RemindersViewController

---
+++ Home Reminders/Controllers/RemindersViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  Home Reminders
+//
+//  Created by Lawrence H. Schoch on 9/6/25.
+//
+
+import UIKit
+
+class RemindersViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var reminders: [Reminder] = [
+        Reminder(description: "Check water softener level", frequency: "2", period: "weeks", dateLast: "2025-09-06", dateNext: "2025-09-20", note: "no note for this reminder"),
+        Reminder(description: "Change furnace filters", frequency: "6", period: "months", dateLast: "2025-06-06", dateNext: "2025-12-06", note: "no note for this reminder")
+        
+        
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        //MARK: - Date Logic
+        let currentDate = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let formattedDate = dateFormatter.string(from: currentDate)
+//        print("Formatted date: \(formattedDate)")
+        
+        //MARK: - Database File
+        // Copy database file to documents directory (one time only), print database file path
+        let docName = "home_reminders"
+        let docExt = "db"
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+        let destURL = documentsURL!.appendingPathComponent(docName).appendingPathExtension(docExt)
+        print(destURL) // Location of home_reminders.db
+        //        copyFileToDocumentsFolder(nameForFile: docName, extForFile: docExt) // Function in Utilities folder, run one time
+        
+        //        loadReminders()
+        
+    }
+}
+   
+extension RemindersViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return reminders.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let reminder = reminders[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReminderCell", for: indexPath)
+        
+        cell.textLabel?.text = reminder.description
+
+        return cell
+    }
+}
+
+extension RemindersViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Selected row: \(indexPath.row)")
+    }
+}
--- Home Reminders/Supporting Files/Utilities.swift
+++ Home Reminders/Supporting Files/Utilities.swift
@@ -23,3 +23,58 @@ func copyFileToDocumentsFolder(nameForFile: String, extForFile: String) {
             print("Unable to copy file")
         }
 }
+
+//{
+//    
+//    @IBOutlet weak var tableView: UITableView!
+//    
+//    var reminders: [Reminder] = [
+//        Reminder(description: "Check water softener level", frequency: "2", period: "weeks", dateLast: "2025-09-06", dateNext: "2025-09-20", note: "no note for this reminder"),
+//        Reminder(description: "Change furnace filters", frequency: "6", period: "months", dateLast: "2025-06-06", dateNext: "2025-12-06", note: "no note for this reminder")
+//    
+//    
+//    ]
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        // Date logic
+//        let currentDate = Date()
+//        let dateFormatter = DateFormatter()
+//        dateFormatter.dateFormat = "yyyy-MM-dd"
+//        let formattedDate = dateFormatter.string(from: currentDate)
+//        print("Formatted date: \(formattedDate)")
+//        
+//        // Copy database file to documents directory (one time only), print database file path
+//        let docName = "home_reminders"
+//        let docExt = "db"
+//        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+//        let destURL = documentsURL!.appendingPathComponent(docName).appendingPathExtension(docExt)
+//        print(destURL) // Location of home_reminders.db
+////        copyFileToDocumentsFolder(nameForFile: docName, extForFile: docExt) // Function in Utilities folder, run one time
+//        
+////        loadReminders()
+//        
+//    }
+//    
+//    
+//    //MARK: - TableView Datasource Methods
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+////        print(reminders.count)
+//        return reminders.count
+//        
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        
+//        let reminder = reminders[indexPath.row]
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "ReminderCell", for: indexPath)
+//        
+//        cell.textLabel?.text = reminder.description
+//
+//        return cell
+//    }
+//
+//}
+//
+
