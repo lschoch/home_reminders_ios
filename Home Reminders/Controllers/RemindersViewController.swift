1e3bba2e2e89217cecf99ad58402bfddeda01b27
Larry Schoch <lschoch@gmail.com>
2025-09-14 14:17:01 -0400

    Connect to home_reminders database, load reminders from database and display them in the reminders table.

--- Home Reminders/Controllers/RemindersViewController.swift
+++ Home Reminders/Controllers/RemindersViewController.swift
@@ -6,17 +6,14 @@
 //
 
 import UIKit
+import SQLite
 
 class RemindersViewController: UIViewController {
     
     
     @IBOutlet weak var tableView: UITableView!
     
-    var reminders: [Reminder] = [
-        Reminder(description: "Check water softener level", frequency: "2", period: "weeks", dateLast: "2025-09-06", dateNext: "2025-09-20", note: "no note for this reminder"),
-        Reminder(description: "Change furnace filters", frequency: "6", period: "months", dateLast: "2025-06-06", dateNext: "2025-12-06", note: "no note for this reminder but what happens if the note is very long?"),
-        Reminder(description: "Change furnace filters", frequency: "6", period: "months", dateLast: "2025-06-06", dateNext: "2025-12-06", note: "no note for this reminder but what happens if the note is very long?")
-    ]
+    var reminders: [Reminder] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,27 +23,51 @@ class RemindersViewController: UIViewController {
         tableView.register(UINib(nibName: "CustomCell", bundle: nil), forCellReuseIdentifier: "CustomCell")
         
         tableView.rowHeight = 160 // UITableView.automaticDimension
-//        tableView.estimatedRowHeight = 130 // Adjust as needed
         
+        loadReminders()
         
-        //MARK: - Date Logic
-//        let currentDate = Date()
-//        let dateFormatter = DateFormatter()
-//        dateFormatter.dateFormat = "yyyy-MM-dd"
-//        let formattedDate = dateFormatter.string(from: currentDate)
-//        print("Formatted date: \(formattedDate)")
-        
-        //MARK: - Database File
+    }
+    
+    //MARK: - Data Manipulation Methods
+    func loadReminders() {
         // Copy database file to documents directory (one time only), print database file path
         let docName = "home_reminders"
         let docExt = "db"
         let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
-        let destURL = documentsURL!.appendingPathComponent(docName).appendingPathExtension(docExt)
-        print(destURL) // Location of home_reminders.db
-        //        copyFileToDocumentsFolder(nameForFile: docName, extForFile: docExt) // Function in Utilities folder, run one time
+        let dbPath = documentsURL!.appendingPathComponent(docName).appendingPathExtension(docExt)
         
-        //        loadReminders()
+        // Connect to database
+        var db: Connection!
+        do {
+            db = try Connection("\(dbPath)")
+        } catch {
+            print("Error opening database: \(error)")
+        }
         
+        // Select all reminders and append each to the reminders array
+        do {
+            let remindersTable = Table("reminders")
+            let description = Expression<String>("description")
+            let frequency = Expression<String>("frequency")
+            let period = Expression<String>("period")
+            let date_last = Expression<String>("date_last")
+            let date_next = Expression<String>("date_next")
+            let note = Expression<String>("note")
+            
+            for reminder in try db.prepare(remindersTable) {
+                reminders.append(Reminder(
+                    description: try reminder.get(description),
+                    frequency: try reminder.get(frequency),
+                    period: try reminder.get(period),
+                    dateLast: try reminder.get(date_last),
+                    dateNext: try reminder.get(date_next),
+                    note: try reminder.get(note)
+                )
+                )
+            }
+        } catch {
+            print("Error during query: \(error)")
+        }
     }
 }
    
